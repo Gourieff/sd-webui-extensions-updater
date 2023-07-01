b2ec7788a88e129a30c2b12de05bb676427dc890
Art Gourieff <85128026+Gourieff@users.noreply.github.com>
2023-07-01 21:41:00 +0700

    Extensions Update Script (Bash)

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Green='\033[0;32m'
+Def='\033[m'
+
+echo -e "${Green}Extensions${Def}"
+git pull
+echo ---
+
+for f in *; do
+    if [ -d "$f" ]; then
+        echo -e "${Green}$f${Def}"
+        cd "$f"
+        git pull
+        echo ---
+        cd ..
+    fi
+done
