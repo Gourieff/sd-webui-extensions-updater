c5e62333d19769130e1a953ac34e6b687755d86c
Art Gourieff <85128026+Gourieff@users.noreply.github.com>
2023-08-19 13:13:15 +0700

    UPDATE: Titles

--- update.sh
+++ update.sh
@@ -3,13 +3,13 @@
 Green='\033[0;32m'
 Def='\033[m'
 
-echo -e "${Green}Extensions${Def}"
+echo -e "${Green}MainApp:${Def}"
 git pull
 echo ---
 
 for f in *; do
     if [ -d "$f" ]; then
-        echo -e "${Green}$f${Def}"
+        echo -e "${Green}$f:${Def}"
         cd "$f"
         git pull
         echo ---
