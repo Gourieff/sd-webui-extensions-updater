22337fb19cc269b67638f4ecf2f0c8f6cb85f709
Art Gourieff <85128026+Gourieff@users.noreply.github.com>
2023-08-19 12:58:55 +0700

    UPDATE: Titles

--- update.bat
+++ update.bat
@@ -1,11 +1,11 @@
 @echo off
 
-call :colored Extensions Green
+call :colored MainApp: Green
 git pull
 echo ---
 
 for /f %%d in ('dir /B /A:D %CD%') do (
-    call :colored %%d Green
+    call :colored %%d: Green
     cd "%CD%\%%d"
     git pull
     echo ---
